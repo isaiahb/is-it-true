851c774a1dcf99ae54005a4c6d28862ac99987d3
isaiahb <isaiahballah@gmail.com>
2022-07-13 08:06:36 -0700

    get port from env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function isItTrue(value) {
   const _value = value.toLowerCase();
